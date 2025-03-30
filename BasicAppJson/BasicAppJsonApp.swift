6b07b9af60318c7021c42caf028b9dd72cee4a48
nabusim <cristianjose@gmail.com>
2025-03-30 19:22:41 +0200

    Initial Commit

---
+++ BasicAppJson/BasicAppJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAppJsonApp.swift
+//  BasicAppJson
+//
+//  Created by Cristian Jose Perez Guerra on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BasicAppJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
